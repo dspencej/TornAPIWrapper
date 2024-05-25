707036a5869a9b03f2dc670736e69b2ffc588695
Dustin Spencer <dspencej@gmail.com>
2024-05-24 20:09:39 -0400

    Updated comments and formatting.

--- src/TornAPIWrapper/torn_api_error_handler.py
+++ src/TornAPIWrapper/torn_api_error_handler.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2023-Present cxdzc
+Copyright (c) 2023-Present cxdzc, dspencej
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -22,6 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+
 class TornApiErrorHandler:
     """
     Handle Torn API errors.
@@ -29,28 +30,40 @@ class TornApiErrorHandler:
     Methods:
     - api_error_handler(response) -> dict: Handle Torn API errors.
     """
-    error_codes = {0: {"Unknown error": "Unhandled error, should not occur."},
-       1: {"Key is empty": "Private key is empty in current request."},
-       2: {"Incorrect Key": "Private key is wrong/incorrect format."},
-       3: {"Wrong type": "Requesting an incorrect basic type."},
-       4: {"Wrong fields": "Requesting incorrect selection fields."},
-       5: {"Too many requests": "Requests are blocked for a small period of time because of too many requests per user (max 100 per minute)."},
-       6: {"Incorrect ID": "Wrong ID value."},
-       7: {"Incorrect ID-entity relation": "A requested selection is private (For example, personal data of another user / faction)."},
-       8: {"IP block": "Current IP is banned for a small period of time because of abuse."},
-       9: {"API disabled": "Api system is currently disabled."},
-       10: {"Key owner is in federal jail": "Current key can't be used because owner is in federal jail."},
-       11: {"Key change error": "You can only change your API key once every 60 seconds."},
-       12: {"Key read error": "Error reading key from Database."},
-       13: {"The key is temporarily disabled due to owner inactivity": "The key owner hasn't been online for more than 7 days."},
-       14: {"Daily read limit reached": "Too many records have been pulled today by this user from our cloud services."},
-       15: {"Temporary error": "An error code specifically for testing purposes that has no dedicated meaning."},
-       16: {"Access level of this key is not high enough": "A selection is being called of which this key does not have permission to access."},
-       17: {"Backend error occurred": "Please try again."}}
+    error_codes = {
+        0: {"Unknown error": "Unhandled error, should not occur."},
+        1: {"Key is empty": "Private key is empty in current request."},
+        2: {"Incorrect Key": "Private key is wrong/incorrect format."},
+        3: {"Wrong type": "Requesting an incorrect basic type."},
+        4: {"Wrong fields": "Requesting incorrect selection fields."},
+        5: {
+            "Too many requests": "Requests are blocked for a small period of time because of too many requests per "
+                                 "user (max 100 per minute)."},
+        6: {"Incorrect ID": "Wrong ID value."},
+        7: {
+            "Incorrect ID-entity relation": "A requested selection is private (For example, personal data of another "
+                                            "user / faction)."},
+        8: {"IP block": "Current IP is banned for a small period of time because of abuse."},
+        9: {"API disabled": "Api system is currently disabled."},
+        10: {"Key owner is in federal jail": "Current key can't be used because owner is in federal jail."},
+        11: {"Key change error": "You can only change your API key once every 60 seconds."},
+        12: {"Key read error": "Error reading key from Database."},
+        13: {
+            "The key is temporarily disabled due to owner inactivity": "The key owner hasn't been online for more "
+                                                                       "than 7 days."},
+        14: {
+            "Daily read limit reached": "Too many records have been pulled today by this user from our cloud services."},
+        15: {"Temporary error": "An error code specifically for testing purposes that has no dedicated meaning."},
+        16: {
+            "Access level of this key is not high enough": "A selection is being called of which this key does not "
+                                                           "have permission to access."},
+        17: {"Backend error occurred": "Please try again."}
+    }
 
     def api_error_handler(self, response) -> dict:
         """
         Handle Torn API errors.
+
         :param response: Response object from `api_request`.
         :return: Json-encoded data.
         """
@@ -58,11 +71,11 @@ class TornApiErrorHandler:
             data = response.json()
             if "error" in data:
                 error_code = data["error"]["code"]
-                if error_code in [0, 1, 2, 5, 8, 9, 10, 11, 12, 13, 14, 16, 17]:
-                    raise Exception(f"API Error Code {error_code}: {self.error_codes[error_code]}")
-                else:
+                if error_code in self.error_codes:
                     error, description = list(self.error_codes[error_code].items())[0]
-                    data = {"error": {"code": error_code, "error": error, "description": description}}
+                    raise Exception(f"API Error Code {error_code}: {error} - {description}")
+                else:
+                    raise Exception(f"API Error Code {error_code}: Unknown error.")
             return data
         else:
             raise Exception(f"Error {response.status_code}: {response.text}")
