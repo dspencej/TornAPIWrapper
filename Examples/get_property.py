ca7a1d030b1407e374e31f613d0a915d4ce36fca
Dustin Spencer <dspencej@gmail.com>
2024-05-24 20:29:24 -0400

    Refactored to get API from environmental variables.

--- Examples/get_property.py
+++ Examples/get_property.py
@@ -1,7 +1,19 @@
+import os
+import logging
+import sys
 from TornAPIWrapper import TornApiWrapper
 
-# Initialize the TornApiWrapper with your API key
-taw = TornApiWrapper(api_key="1aBcDeFgH2iJkLmN")  # Insert your API key
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Get API key from environment variable
+api_key = os.getenv('TORN_API')
+if not api_key:
+    logging.error("Environment variable TORN_API not set. Please set the TORN_API environment variable.")
+    sys.exit(1)
+
+# Initialize the TornApiWrapper with the API key
+taw = TornApiWrapper(api_key=api_key)
 
 # Get Torn City property data for property with ID 752227 and specific selections
 data = taw.get_property(752227, selections=["property", "timestamp"])
--- Examples/get_user.py
+++ Examples/get_user.py
@@ -1,7 +1,19 @@
+import os
+import logging
+import sys
 from TornAPIWrapper import TornApiWrapper
 
-# Initialize the TornApiWrapper with your API key
-taw = TornApiWrapper(api_key="1aBcDeFgH2iJkLmN")  # Insert your API key
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Get API key from environment variable
+api_key = os.getenv('TORN_API')
+if not api_key:
+    logging.error("Environment variable TORN_API not set. Please set the TORN_API environment variable.")
+    sys.exit(1)
+
+# Initialize the TornApiWrapper with the API key
+taw = TornApiWrapper(api_key=api_key)
 
 # Get Torn City user data
 data = taw.get_user()
